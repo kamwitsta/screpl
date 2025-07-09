43714c20a7b602233b5ac79fbd3921d51690747d
Kamil Stachowski <kamil.stachowski@gmail.com>
2025-07-09 21:22:15 +0200

    added dev.clj

---
+++ dev/dev.clj
@@ -0,0 +1,15 @@
+; anything to do with the tree needs to be tested with
+; clj -J-Xmx10M -M a.clj
+; (memory limited to 10MB) to make sure it is properly lazy
+
+(ns dev
+  (:require [clojure.tools.namespace.repl :refer [refresh]]
+            ; [screpl.a :refer [start-gui stop-gui]]))
+            [screpl.gui :refer [start-gui stop-gui]]))
+
+(stop-gui)
+(refresh)
+(start-gui)
+
+(comment
+  (require '[clojure.tools.namespace.repl :refer [refresh]]) (refresh))
